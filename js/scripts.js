8d17673d8916770b95b2581798c906cc1e9656dd
Alex Efremov <alex.efremov.dev@gmail.com>
2016-06-02 23:21:07 +0300

    Add imperial to metric toggle button

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,7 @@
   }
 
   function getWeather(locationData) {
-    var units = "&units=metric";
-    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + locationData.city + "," + locationData.country + units + "&appid=74ab00f9f5d6f488185edff7e764b725";
+    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + locationData.city + "," + locationData.country + '&units=metric&appid=74ab00f9f5d6f488185edff7e764b725';
     var requestWeather = new XMLHttpRequest();
     requestWeather.open('GET', requestUrl, true);
 
@@ -29,10 +28,28 @@
   }
 
   function renderPage(currentWeather) {
+    var convertButton = document.getElementById('convert');
+    var weatherTemp = document.getElementById('weather-temp');
+    var metric = true;
+    var tempCelsius = Math.round(currentWeather.main.temp);
+    var tempFahrenheit = Math.round(currentWeather.main.temp * 9/5 + 32);
     document.getElementById('weather-city').innerHTML = currentWeather.name + ", " + currentWeather.sys.country;
-    document.getElementById('weather-temp').innerHTML = Math.round(currentWeather.main.temp) + "°C";
+    weatherTemp.innerHTML = tempCelsius + '°C';
     document.getElementById('weather-description').innerHTML = currentWeather.weather[0].main;
     document.getElementById('weather-icon').innerHTML = getIconString(currentWeather.weather[0].id);
+    document.getElementById('clothes').innerHTML = '<img src="' + getClothesImage(tempCelsius) + '" alt="Clothes for wearing at ' + tempCelsius + ' Celsius">';
+    convertButton.addEventListener('click', function() {
+      if (metric === true) {
+        weatherTemp.innerHTML = tempFahrenheit + '°F';
+        convertButton.textContent = 'To Metric';
+        metric = false;
+      }
+      else {
+        weatherTemp.innerHTML = tempCelsius + '°C';
+        convertButton.textContent = 'To Imperial';
+        metric = true;
+      }
+    });
   }
 
   function getIconString(weatherId) {
